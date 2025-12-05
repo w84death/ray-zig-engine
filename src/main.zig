925a07808bddfe90350ececed9fa00871b919d91
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-12-06 00:21:30 +0100

    alpha version

---
+++ src/main.zig
@@ -0,0 +1,133 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const WINDOW_WIDTH = 640;
+const WINDOW_HEIGHT = 480;
+pub const DB16 = struct {
+    pub const BLACK       = rl.Color{ .r = 20,  .g = 12,  .b = 28,  .a = 255 };
+    pub const PURPLE      = rl.Color{ .r = 68,  .g = 36,  .b = 52,  .a = 255 };
+    pub const RED         = rl.Color{ .r = 148, .g = 0,   .b = 52,  .a = 255 };
+    pub const BROWN       = rl.Color{ .r = 116, .g = 60,  .b = 0,   .a = 255 };
+    pub const DARK_GREEN  = rl.Color{ .r = 0,   .g = 92,  .b = 0,   .a = 255 };
+    pub const DARK_GRAY   = rl.Color{ .r = 76,  .g = 76,  .b = 84,  .a = 255 };
+    pub const GRAY        = rl.Color{ .r = 136, .g = 136, .b = 136, .a = 255 };
+    pub const WHITE       = rl.Color{ .r = 248, .g = 248, .b = 248, .a = 255 };
+    pub const LIGHT_RED   = rl.Color{ .r = 248, .g = 56,  .b = 0,   .a = 255 };
+    pub const ORANGE      = rl.Color{ .r = 228, .g = 92,  .b = 16,  .a = 255 };
+    pub const YELLOW      = rl.Color{ .r = 248, .g = 216, .b = 120, .a = 255 };
+    pub const GREEN       = rl.Color{ .r = 0,   .g = 168, .b = 0,   .a = 255 };
+    pub const CYAN        = rl.Color{ .r = 0,   .g = 184, .b = 248, .a = 255 };
+    pub const BLUE        = rl.Color{ .r = 0,   .g = 88,  .b = 248, .a = 255 };
+    pub const LIGHT_BLUE  = rl.Color{ .r = 136, .g = 180, .b = 248, .a = 255 };
+    pub const PINK        = rl.Color{ .r = 248, .g = 120, .b = 248, .a = 255 };
+};
+
+const Vec2 = struct {
+    x: i32,
+    y: i32,
+
+    pub fn init(x: i32, y: i32,) Vec2 {
+      return .{.x = x, .y = y};
+    }
+
+    pub fn add(self: Vec2, other: Vec2) Vec2 {
+      return .{ .x = self.x + other.x, .y = self.y + other.y };
+    }
+};
+
+pub const Block = struct {
+  pos: Vec2,
+  size: Vec2,
+  vel: Vec2 = Vec2.init(0, 0),
+  color: rl.Color,
+  base_color: rl.Color,
+
+  pub fn init(x: i32, y: i32, w: i32, h: i32, color: rl.Color) Block {
+    return .{
+      .pos = Vec2.init(x, y),
+      .size = Vec2.init(w, h),
+      .color = color,
+      .base_color = color,
+    };
+  }
+
+  pub fn getRect(self: Block) struct {x: i32, y: i32, w: i32, h: i32} {
+    return .{
+      .x = self.pos.x,
+      .y = self.pos.y,
+      .w = self.size.x,
+      .h = self.size.y,
+    };
+  }
+
+  pub fn draw(self: Block) void {
+    rl.drawRectangle(self.pos.x, self.pos.y, self.size.x, self.size.y, self.color);
+  }
+
+  pub fn update(self: *Block) void {
+    const new_pos = Vec2.init(self.pos.x + self.vel.x, self.pos.y + self.vel.y);
+    if (new_pos.x > 0 and new_pos.y > 0 and new_pos.x + self.size.x <= WINDOW_WIDTH and new_pos.y + self.size.y <= WINDOW_HEIGHT) {
+      self.pos = self.pos.add(self.vel);
+    }
+  }
+
+  pub fn bounce(self: *Block) void {
+    const new_pos = Vec2.init(self.pos.x + self.vel.x, self.pos.y + self.vel.x);
+    if(new_pos.x <= 0 or new_pos.x + self.size.x >= WINDOW_WIDTH){
+      self.vel.x = -self.vel.x;
+    }
+
+    if(new_pos.y <= 0 or new_pos.y + self.size.y >= WINDOW_HEIGHT) {
+      self.vel.y = -self.vel.y;
+    }
+
+    self.update();
+  }
+
+  pub fn setVelocity(self: *Block, vx: i32, vy: i32) void {
+    self.vel = Vec2.init(vx, vy);
+  }
+
+  pub fn collideWidth(self: Block, other: Block) bool {
+    const a = self.getRect();
+    const b = other.getRect();
+    return a.x < b.x + b.w and
+      a.x + a.w > b.x and
+      a.y < b.y + b.h and
+      a.y + a.h > b.y;
+  }
+};
+
+pub fn main() !void {
+  rl.initWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Zig/Raylib Engine");
+  defer rl.closeWindow();
+  rl.setTargetFPS(60);
+  const title_pos = Vec2.init(10,10);
+
+  var player = Block.init(100, 100, 50, 50, DB16.YELLOW);
+  var enemy = Block.init(200, 200, 50, 50, DB16.PINK);
+
+  player.setVelocity(1, 0);
+  enemy.setVelocity(-1, 0);
+
+  while(rl.windowShouldClose() == false) {
+    player.update();
+    enemy.bounce();
+
+    const colliding = player.collideWidth(enemy);
+
+    rl.beginDrawing();
+    defer rl.endDrawing();
+
+    rl.clearBackground(DB16.LIGHT_BLUE);
+    rl.drawRectangle(title_pos.x-8, title_pos.y-8, 256, 32, DB16.YELLOW);
+    rl.drawText("Zig/Raylib Engine", title_pos.x, title_pos.y, 20, DB16.BLUE);
+
+    player.draw();
+    enemy.draw();
+
+    if (colliding) {
+      rl.drawText("COLLISION!", 250, 60, 32, DB16.RED);
+    }
+  }
+}
