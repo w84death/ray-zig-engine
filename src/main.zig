77a50f62f277a35fcc0f4263f66eb7ed484bcbfc
Krzysztof Krystian Jankowski <kj@p1x.in>
2025-12-06 18:47:51 +0100

    gravity, fps

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,10 @@ const rl = @import("raylib");
 
 const WINDOW_WIDTH = 640;
 const WINDOW_HEIGHT = 480;
+const TERRAIN_WIDTH = 256;
+const TERRAIN_HEIGHT = 256;
+const GRAVITY = 98.1;
+
 pub const DB16 = struct {
     pub const BLACK = rl.Color{ .r = 20, .g = 12, .b = 28, .a = 255 };
     pub const PURPLE = rl.Color{ .r = 68, .g = 36, .b = 52, .a = 255 };
@@ -40,6 +44,10 @@ const Vec2 = struct {
     pub fn mul(self: Vec2, other: Vec2) Vec2 {
         return .{ .x = self.x * other.x, .y = self.y * other.y };
     }
+
+    pub fn clamp(self: Vec2, min: f32, max: f32) Vec2 {
+        return .{ .x = std.math.clamp(self.x, min, max), .y = std.math.clamp(self.y, min, max) };
+    }
 };
 
 pub const IVec2 = struct {
@@ -66,12 +74,15 @@ pub const Block = struct {
     pos: Vec2,
     size: IVec2,
     vel: Vec2 = Vec2.init(0, 0),
+    max_vel: f32 = 250.0,
+    speed: f32 = 0.0,
     color: rl.Color,
 
-    pub fn init(x: f32, y: f32, w: i32, h: i32, color: rl.Color) Block {
+    pub fn init(x: f32, y: f32, w: i32, h: i32, speed: f32, color: rl.Color) Block {
         return .{
             .pos = Vec2.init(x, y),
             .size = IVec2.init(w, h),
+            .speed = speed,
             .color = color,
         };
     }
@@ -90,6 +101,7 @@ pub const Block = struct {
     }
 
     pub fn update(self: *Block, dt: f32) void {
+        self.vel.y += GRAVITY * dt;
         const new_pos = self.pos.add(.{ .x = self.vel.x * dt, .y = self.vel.y * dt });
         const window_w: f32 = @floatFromInt(WINDOW_WIDTH);
         const window_h: f32 = @floatFromInt(WINDOW_HEIGHT);
@@ -112,10 +124,17 @@ pub const Block = struct {
     pub fn bounceOff(self: *Block) void {
         self.vel.x = -self.vel.x;
         self.vel.y = -self.vel.y;
+        self.vel.x *= 0.25;
+        self.vel.y *= 0.25;
     }
 
     pub fn setVelocity(self: *Block, vx: f32, vy: f32) void {
         self.vel = Vec2.init(vx, vy);
+    }
+
+    pub fn addVel(self: *Block, vel: Vec2) void {
+        self.vel = self.vel.add(Vec2.init(vel.x * self.speed, vel.y * self.speed));
+        self.vel = self.vel.clamp(-self.max_vel, self.max_vel);
     }
 
     pub fn collidesWidth(self: Block, other: Block) bool {
@@ -149,16 +168,22 @@ pub fn main() !void {
 
     rl.setTargetFPS(60);
 
-    var player = Entity.init(Block.init(100, 100, 24, 24, DB16.BLACK), EntityType.Player, 100);
-    var enemy = Entity.init(Block.init(200, 200, 12, 12, DB16.PURPLE), EntityType.Enemy, 10);
-    var enemy2 = Entity.init(Block.init(300, 300, 12, 12, DB16.ORANGE), EntityType.Enemy, 10);
+    // var terrain = Terrain.init();
+    var player = Entity.init(Block.init(100, 100, 24, 24, 100.0, DB16.BLACK), EntityType.Player, 100);
+    var enemy = Entity.init(Block.init(200, 200, 12, 12, 50.0, DB16.PURPLE), EntityType.Enemy, 10);
+    var enemy2 = Entity.init(Block.init(300, 300, 12, 12, 50.0, DB16.ORANGE), EntityType.Enemy, 10);
 
-    player.block.setVelocity(20.0, -30.0);
     enemy.block.setVelocity(-40.0, -80.0);
     enemy2.block.setVelocity(60.0, -40.0);
 
     while (rl.windowShouldClose() == false) {
         const dt: f32 = rl.getFrameTime();
+
+        if (rl.isKeyDown(rl.KeyboardKey.right)) player.block.addVel(Vec2.init(dt, 0.0));
+        if (rl.isKeyDown(rl.KeyboardKey.left)) player.block.addVel(Vec2.init(-dt, 0.0));
+        if (rl.isKeyDown(rl.KeyboardKey.up)) player.block.addVel(Vec2.init(0.0, -dt));
+        if (rl.isKeyDown(rl.KeyboardKey.down)) player.block.addVel(Vec2.init(0.0, dt));
+
         player.block.update(dt);
         enemy.block.update(dt);
         enemy2.block.update(dt);
@@ -170,8 +195,17 @@ pub fn main() !void {
 
         rl.clearBackground(DB16.LIGHT_BLUE);
 
-        rl.drawRectangle(2, 2, WINDOW_WIDTH - 2, 32, DB16.YELLOW);
-        rl.drawText("Zig/Raylib Engine", 8, 8, 20, DB16.BLUE);
+        // terrain.draw();
+
+        rl.drawRectangle(2, 2, 38, 32, DB16.YELLOW);
+        var fps_buffer: [32]u8 = undefined;
+        const fps_text = std.fmt.bufPrintZ(&fps_buffer, "{d}", .{rl.getFPS()}) catch "0";
+        rl.drawText(fps_text, 8, 8, 20, DB16.BLACK);
+
+        rl.drawRectangle(50, 2, player.health * 5, 32, DB16.WHITE);
+        var health_buffer: [32]u8 = undefined;
+        const health_text = std.fmt.bufPrintZ(&health_buffer, "{d}", .{player.health}) catch "0";
+        rl.drawText(health_text, 54, 8, 20, DB16.BLACK);
 
         player.block.draw();
         enemy.block.draw();
@@ -179,6 +213,11 @@ pub fn main() !void {
 
         if (colliding) {
             player.block.bounceOff();
+            player.health -= 1;
+            if (player.health <= 0) {
+                player.block.pos = Vec2.init(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+                player.health = 100;
+            }
         }
     }
 }
